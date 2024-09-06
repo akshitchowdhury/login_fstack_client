d07789aba6898e8d451d512de784e41e3a90d231
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-09-06 14:04:56 +0530

    ServerApi3

--- src/component/LogoutButton.jsx
+++ src/component/LogoutButton.jsx
@@ -5,7 +5,7 @@ const LogoutButton = () => {
   const { logout } = useAuth0();
 
   return (
-    <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
+    <button onClick={() => logout({ logoutParams: { returnTo: "https://dev-xp6lkzqc7iypvpsw.us.auth0.com/u/login?state=hKFo2SB3dVV2bVZNdjJERFl5RGJQMDdLc3NOSUdkVDdVMzl5X6Fur3VuaXZlcnNhbC1sb2dpbqN0aWTZIHdnY3kydUZ2MkhzLU5xY1FZcXhwUUxOa2RqTWY0T1dFo2NpZNkgV084cFpKQURsV0xDcEVEZWNyQmZncHZOakZUcWwyckM" } })}>
       Log Out
     </button>
   );
