0831e7ea236ead93dc12c6d5b6937e0981440207
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-09-06 14:08:43 +0530

    Login

--- src/component/Posts.jsx
+++ src/component/Posts.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Card } from 'shadcn-react'
 import InputForm from './InputForm'
+import Profile from './Profile'
 
 const Posts = () => {
     const [post, setPosts] = useState([])
@@ -34,6 +35,7 @@ const Posts = () => {
 
     return (
         <>
+        <Profile/>
         <div className='max-w-4xl mx-auto p-4'>
             <h1 className='text-5xl text-center font-bold mb-8'>User List</h1>
 
